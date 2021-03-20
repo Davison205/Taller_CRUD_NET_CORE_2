651ac1bd83f50fc3c43a7ceb239fb50b85a092fe
Davison <davisontobon2019@gmail.com>
2021-03-20 11:20:06 -0500

    Se agrega la clase Proveedor

---
+++ Taller_CRUD_NET_CORE_2.git/Models/Entities/Proveedor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller_CRUD_NET_CORE_2.git.Models.Entities
+{
+    public class Proveedor
+    {
+        [Key]
+        public int ProveedorId { get; set; }
+        public String Nombre { get; set; }
+        public String PersonaContacto { get; set; }
+        public String Correo { get; set; }
+        public String Telefono { get; set; }
+
+
+
+    }
+}
