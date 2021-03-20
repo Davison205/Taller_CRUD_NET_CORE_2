3815acc24086f805ce663a20370b4bafb932ca36
Davison <davisontobon2019@gmail.com>
2021-03-20 10:33:46 -0500

    Agregar archivos de proyecto.

---
+++ Taller_CRUD_NET_CORE_2.git/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Taller_CRUD_NET_CORE_2.git.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
